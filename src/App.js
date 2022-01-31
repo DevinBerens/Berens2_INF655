5b38d11ebf5ec4ba9fbdecb9b38da59c5f6c9d08
DevinBerens <devinlberens3@gmail.com>
2022-01-31 10:24:13 -0600

    finished assignment 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let name = 'Devin';
+
+  let tasks = [
+    {
+      id: 1,
+      title: 'Homework',
+      description: 'There are 3 assignments to do this week'
+    },
+    {
+      id: 2,
+      title: 'Wash dishes',
+      description: 'You need to wash the dishes'
+    },
+    {
+      id: 3,
+      title: 'Do laundry',
+      description: 'It is time to do laundry'
+    }
+  ];
+
+  let generateTask = () => {
+    let num = Math.floor(Math.random() * (tasks.length - 0) + 0);
+    return tasks[num].title;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      My name is <h3>{name}</h3> and the selected task to do is <h3>{generateTask()}</h3>
     </div>
   );
 }
